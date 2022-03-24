0ca58ac3a76d203ba351e6a498fa5c5881b5e7fd
shivam <shivamweb2@gmail.com>
2022-03-24 19:01:07 +0530

    keeper project  added

--- src/data.js
+++ src/data.js
@@ -18,15 +18,15 @@ export const products = [
     id: 3,
     img: "https://d33wubrfki0l68.cloudfront.net/623a19e3226c6b00089f5590/screenshot_2022-03-22-18-48-16-0000.png",
     link: "http://lama.dev",
-    code: "https://github.com/shivamvr/project-instagram",
+    code: "https://github.com/shivamvr/blog-wesite",
     tstack: ["html", "css", "react", "sass", "javascript"]
 
   },
   {
     id: 4,
-    img: "https://d33wubrfki0l68.cloudfront.net/623a19e3226c6b00089f5590/screenshot_2022-03-22-18-48-16-0000.png",
-    link: "http://lama.dev",
-    code: "https://github.com/shivamvr/project-instagram",
-    tstack: ["html", "css","javascript","heroku","json"]
+    img: "https://d33wubrfki0l68.cloudfront.net/623c6e68656dea0009a46a37/screenshot_2022-03-24-13-13-37-0000.png",
+    link: "https://keepite.netlify.app/",
+    code: "https://github.com/shivamvr/KeeperApp",
+    tstack: ["html", "css","javascript",'react']
   },
 ];
