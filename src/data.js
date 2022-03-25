8149fe94bd53d8b2bb8ca06f8369633384bd91cd
shivam <shivamweb2@gmail.com>
2022-03-25 20:17:32 +0530

    minor changes

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export const products = [
   {
     id: 1,
-    img: "https://d33wubrfki0l68.cloudfront.net/619feb31d5fb9a2ca5be1458/screenshot_2021-11-25-20-01-31-0000.png",
+    img: "/icons/music.png",
     link: "https://neomusic.netlify.app/",
     code: "https://github.com/shivamvr/MusicPlayer",
     tstack: ["html", "css", "react", "sass", "javascript","json","heroku"]
   },
   {
     id: 2,
-    img: "https://d33wubrfki0l68.cloudfront.net/622b14b53dd07d0009812eaa/screenshot_2022-03-11-09-22-38-0000.png",
+    img: "/icons/insta.png",
     link: "https://likeinstagram.netlify.app/",
     code: "https://github.com/shivamvr/project-instagram",
     tstack: ["html", "css", "react", "sass", "javascript","json","heroku","styled"]
@@ -32,7 +32,6 @@ export const products = [
   },
   {
     id: 5,
-    img: "/icons/keeper.png",
     link: "https://keepite.netlify.app/",
     code: "https://github.com/shivamvr/KeeperApp",
     tstack: ["html", "css","javascript",'react']
