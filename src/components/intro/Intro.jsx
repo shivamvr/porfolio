da28bcf250b0605d826157a8aa6b17e3f57b1633
shivamvr <shivamweb2@gmail.com>
2022-09-12 14:58:32 +0530

    resume updated

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -24,7 +24,7 @@ const Intro = () => {
             <a
               className='resume'
               href='
-https://drive.google.com/file/d/1AmOFlgkfQUt7PH4nDasgMYHoGsN1wXzM/view?usp=sharing
+              https://drive.google.com/file/d/1Wr434g1003JAv5jB8rby4KLKLrVvaTwc/view?usp=sharing
           '
               rel='noopener noreferrer'
               target='_blank'
